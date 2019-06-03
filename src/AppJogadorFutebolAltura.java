ad9e70aab5e5cf7b470e14d8d6e2ac92f88d327a
Eraldo R. Fernandes <eraldoluis@gmail.com>
2019-06-03 15:24:03 -0400

    Primeiro commit.

---
+++ src/AppJogadorFutebolAltura.java
@@ -0,0 +1,155 @@
+
+import java.util.List;
+
+import org.eclipse.rdf4j.model.IRI;
+import org.eclipse.rdf4j.model.Literal;
+import org.eclipse.rdf4j.query.BindingSet;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+/**
+ * Aplicação exibe lista de países da América do Sul. Quando o usuário seleciona
+ * um país, a aplicação consulta as alturas de todos os jogadores de futebol
+ * (rdf:type = dbo:SoccerPlayer) deste país e exibe o jogador mais baixo, o mail
+ * alto, a altura média e o número de jogadores encontrados.
+ * 
+ * @author Eraldo R. Fernandes (eraldo@facom.ufms.br)
+ *
+ */
+public class AppJogadorFutebolAltura extends Application {
+	/**
+	 * Janela principal da aplicação.
+	 */
+	private Stage janelaPrincipal;
+
+	/**
+	 * ComboBox para exibição dos países da América do Sul.
+	 */
+	private ComboBox<String> cbPaises;
+
+	@Override
+	public void start(Stage primaryStage) {
+		// Referência para a janela principal.
+		janelaPrincipal = primaryStage;
+
+		// Título da janela.
+		janelaPrincipal.setTitle("Altura de Jogadores");
+
+		// Botão de busca: chama método buscar() quando for clicado.
+		Button btnBuscar = new Button("Buscar altura dos jogadores");
+		btnBuscar.setOnMouseClicked(e -> buscar());
+
+		// Controle que exibe a lista de países.
+		cbPaises = new ComboBox<>();
+
+		// Obtém lista da combobox.
+		List<String> paises = cbPaises.getItems();
+
+		// Obtém lista de países da DBpedia e preenche combobox.
+		for (BindingSet bs : ConsultaDBPedia.getPaisesAmericaDoSul())
+			paises.add(bs.getValue("pais").toString());
+
+		// Cria container vertical (existem vários outros) e insere os controles.
+		VBox container = new VBox(new Label("Selecione um país:"), cbPaises, btnBuscar);
+
+		// Inclui container na janela principal.
+		janelaPrincipal.setScene(new Scene(container));
+
+		// Exibe janela principal.
+		janelaPrincipal.show();
+	}
+
+	/**
+	 * Obtém todos os jogadores de futebol do país selecionado pelo usuário e exibe
+	 * o jogador mais alto, o mais baixo, a altura média e o número total de
+	 * jogadores considerados.
+	 */
+	private void buscar() {
+		// Obtém país selecionado. Este valor é uma IRI completa.
+		String pais = cbPaises.getValue();
+
+		// Variáveis para armazenar os resultados da busca.
+		double min = Double.MAX_VALUE, max = Double.MIN_NORMAL, avg = 0;
+		String sMin = null, sMax = null;
+
+		// Contador de jogadores.
+		int count = 0;
+
+		/*
+		 * Como o método de busca de países retorna IRIs completos (sem usar os
+		 * prefixos), é necessário envolver este valor entre '<' e '>' para inserirmos
+		 * ele em uma consulta SPARQL.
+		 */
+		pais = "<" + pais + ">";
+
+		// Obtém e processa todos os jogadores do país.
+		for (BindingSet bs : ConsultaDBPedia.getJogadoresComAlturaNascidosNoPais(pais)) {
+			/*
+			 * O método usado acima retorna dois valores para cada jogador: o IRI do jogador
+			 * e sua altura.
+			 * 
+			 * Do IRI, usaremos apenas o 'local name' que é aquele nome que aparece depois
+			 * do domínio da DPpedia (ou da Wikipedia), por exemplo, 'Lionel_Messi'.
+			 * 
+			 * A altura é um literal do tipo double. Para converter este valor para um
+			 * double Java, precisamos realizar as operações abaixo.
+			 */
+			String person = ((IRI) bs.getValue("person")).getLocalName();
+			double height = ((Literal) bs.getValue("height")).doubleValue();
+
+			/*
+			 * Filtramos valores incorretos. Existem alguns jogadores com altura igual a
+			 * zero e outros com a altura em centimétros ao invés de metros. Estes valores
+			 * deixam os resultaos incorretos. Portanto, vamos desonsiderar valores abaixo
+			 * de 1m ou acima de 3m.
+			 */
+			if (height > 3 || height < 1)
+				continue;
+
+			// Computa os valores que queremos exibir.
+			avg += height;
+			if (height < min) {
+				min = height;
+				sMin = person;
+			}
+			if (height > max) {
+				max = height;
+				sMax = person;
+			}
+			++count;
+		}
+
+		// Média das alturas.
+		avg = avg / count;
+
+		// Cria mensagem com as informações calculadas.
+		String msg = "Número de jogadores: %d\nMédia de altura: %f\n";
+		msg += "Jogador mais baixo: %s (%f)\n";
+		msg += "Jogador mais alto: %s (%f)";
+		msg = String.format(msg, count, avg, sMin, min, sMax, max);
+
+		// Exibe mensagem em uma janela popup.
+		Alert alert = new Alert(AlertType.INFORMATION);
+		alert.setTitle("Pesquisa Concluída");
+		alert.setHeaderText(String.format("Dados de %s", pais));
+		alert.setContentText(msg);
+		alert.showAndWait();
+	}
+
+	/**
+	 * Lança aplicação.
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ src/ConsultaDBPedia.java
@@ -0,0 +1,214 @@
+import java.util.List;
+
+import org.eclipse.rdf4j.model.IRI;
+import org.eclipse.rdf4j.model.Literal;
+import org.eclipse.rdf4j.model.Statement;
+import org.eclipse.rdf4j.model.ValueFactory;
+import org.eclipse.rdf4j.model.vocabulary.FOAF;
+import org.eclipse.rdf4j.model.vocabulary.RDF;
+import org.eclipse.rdf4j.model.vocabulary.RDFS;
+import org.eclipse.rdf4j.query.BindingSet;
+import org.eclipse.rdf4j.query.QueryResults;
+import org.eclipse.rdf4j.query.TupleQuery;
+import org.eclipse.rdf4j.query.TupleQueryResult;
+import org.eclipse.rdf4j.repository.Repository;
+import org.eclipse.rdf4j.repository.RepositoryConnection;
+import org.eclipse.rdf4j.repository.RepositoryResult;
+import org.eclipse.rdf4j.repository.sparql.SPARQLRepository;
+
+/**
+ * Provê diversos métodos de consulta da DBpedia usando SPARQL juntamente com a
+ * biblioeteca rdf4j.
+ * 
+ * @author Eraldo R. Fernandes (eraldo@facom.ufms.br)
+ *
+ */
+public class ConsultaDBPedia {
+	private static String getPrefixes() {
+		String prefixes = "";
+		prefixes += "PREFIX foaf: <" + FOAF.NAMESPACE + "> \n";
+		prefixes += "PREFIX rdf: <" + RDF.NAMESPACE + "> \n";
+		prefixes += "PREFIX rdfs: <" + RDFS.NAMESPACE + "> \n";
+		prefixes += "PREFIX dbo: <http://dbpedia.org/ontology/> \n";
+		prefixes += "PREFIX dbr: <http://dbpedia.org/resource/> \n";
+		prefixes += "PREFIX dct: <http://purl.org/dc/terms/> \n";
+		prefixes += "PREFIX dbc: <http://dbpedia.org/resource/Category:> \n";
+		return prefixes;
+	}
+
+	public static List<BindingSet> getJogadoresNascidosEmCampoGrande() {
+		Repository repo = new SPARQLRepository("http://dbpedia.org/sparql");
+		repo.init();
+
+		try (RepositoryConnection conn = repo.getConnection()) {
+			String queryString = getPrefixes();
+
+			queryString += "SELECT DISTINCT ?person WHERE {";
+			queryString += "	?person rdf:type dbo:SoccerPlayer .";
+			queryString += "	?person dbo:birthPlace dbr:Campo_Grande .";
+			queryString += "}";
+
+			TupleQuery query = conn.prepareTupleQuery(queryString);
+
+			try (TupleQueryResult result = query.evaluate()) {
+				return QueryResults.asList(result);
+			}
+		} finally {
+			repo.shutDown();
+		}
+	}
+
+	public static List<BindingSet> getJogadoresNascidosEmMS() {
+		Repository repo = new SPARQLRepository("http://dbpedia.org/sparql");
+		repo.init();
+
+		try (RepositoryConnection conn = repo.getConnection()) {
+			String queryString = getPrefixes();
+
+			queryString += "SELECT DISTINCT ?person WHERE {";
+			queryString += "	?person rdf:type dbo:SoccerPlayer .";
+			queryString += "	?person dbo:birthPlace ?city .";
+			queryString += "	?city dbo:isPartOf dbr:Mato_Grosso_do_Sul .";
+			queryString += "}";
+
+			TupleQuery query = conn.prepareTupleQuery(queryString);
+
+			try (TupleQueryResult result = query.evaluate()) {
+				return QueryResults.asList(result);
+			}
+		} finally {
+			repo.shutDown();
+		}
+	}
+
+	public static List<BindingSet> getJogadoresNascidosNoPais(String pais) {
+		Repository repo = new SPARQLRepository("http://dbpedia.org/sparql");
+		repo.init();
+
+		try (RepositoryConnection conn = repo.getConnection()) {
+			String queryString = getPrefixes();
+
+			queryString += "SELECT DISTINCT ?person WHERE {";
+			queryString += "	?person rdf:type dbo:SoccerPlayer .";
+			queryString += "	?person dbo:birthPlace ?city .";
+			queryString += "	?city dbo:country " + pais + " .";
+			queryString += "}";
+
+			TupleQuery query = conn.prepareTupleQuery(queryString);
+
+			try (TupleQueryResult result = query.evaluate()) {
+				return QueryResults.asList(result);
+			}
+		} finally {
+			repo.shutDown();
+		}
+	}
+
+	public static double getAlturaJogador(IRI jogador) {
+		Repository repo = new SPARQLRepository("http://dbpedia.org/sparql");
+		repo.init();
+
+		try (RepositoryConnection conn = repo.getConnection()) {
+			ValueFactory vf = repo.getValueFactory();
+			RepositoryResult<Statement> rr = conn.getStatements(jogador,
+					vf.createIRI("http://dbpedia.org/ontology/", "height"), null);
+			if (rr.hasNext())
+				return ((Literal) rr.next().getObject()).doubleValue();
+			return Double.NaN;
+		} finally {
+			repo.shutDown();
+		}
+	}
+
+	public static List<BindingSet> getJogadoresComAlturaNascidosNoPais(String pais) {
+		Repository repo = new SPARQLRepository("http://dbpedia.org/sparql");
+		repo.init();
+
+		try (RepositoryConnection conn = repo.getConnection()) {
+			String queryString = getPrefixes();
+
+			queryString += "SELECT DISTINCT ?person ?height WHERE {";
+			queryString += "	?person rdf:type dbo:SoccerPlayer .";
+			queryString += "	?person dbo:birthPlace ?city .";
+			queryString += "	?person dbo:height ?height .";
+			queryString += "	?city dbo:country " + pais + " .";
+			queryString += "}";
+
+			TupleQuery query = conn.prepareTupleQuery(queryString);
+
+			try (TupleQueryResult result = query.evaluate()) {
+				return QueryResults.asList(result);
+			}
+		} finally {
+			repo.shutDown();
+		}
+	}
+
+	public static List<BindingSet> getPaisesAmericaDoSul() {
+		Repository repo = new SPARQLRepository("http://dbpedia.org/sparql");
+		repo.init();
+
+		try (RepositoryConnection conn = repo.getConnection()) {
+			String queryString = getPrefixes();
+
+			queryString += "SELECT DISTINCT ?pais WHERE {";
+			queryString += "	?pais rdf:type dbo:Country .";
+			queryString += "	?pais dct:subject dbc:Countries_in_South_America .";
+			queryString += "}";
+
+			TupleQuery query = conn.prepareTupleQuery(queryString);
+
+			try (TupleQueryResult result = query.evaluate()) {
+				return QueryResults.asList(result);
+			}
+		} finally {
+			repo.shutDown();
+		}
+	}
+
+	public static void main(String[] args) {
+		// for (BindingSet bs : t.getJogadoresNascidosEmCampoGrande()) {
+		// for (BindingSet bs : t.getJogadoresNascidosEmMS()) {
+//			for (BindingSet bs : t.getJogadoresNascidosNoPais("dbr:Brazil")) {
+//				System.out.println(bs.getValue("person"));
+//			}
+
+//			for (BindingSet bs : t.getJogadoresNascidosNoPais("dbr:Brazil")) {
+//				IRI person = (IRI) bs.getValue("person");
+//				System.out.println(person.getLocalName() + " : " + t.getAlturaJogador(person));
+//			}
+
+//			for (BindingSet bs : t.getJogadoresComAlturaNascidosNoPais("dbr:Brazil")) {
+//				IRI person = (IRI) bs.getValue("person");
+//				double height = ((Literal) bs.getValue("height")).doubleValue();
+//				System.out.println(person.getLocalName() + " : " + height);
+//			}
+
+		for (BindingSet bsPais : getPaisesAmericaDoSul()) {
+			double min = Double.MAX_VALUE, max = Double.MIN_NORMAL, avg = 0;
+			String sMin = null, sMax = null;
+			int count = 0;
+			for (BindingSet bs : getJogadoresComAlturaNascidosNoPais("<" + bsPais.getValue("pais") + ">")) {
+				String person = ((IRI) bs.getValue("person")).getLocalName();
+				double height = ((Literal) bs.getValue("height")).doubleValue();
+				if (height > 3 || height < 1)
+					continue;
+				avg += height;
+				if (height < min) {
+					min = height;
+					sMin = person;
+				}
+				if (height > max) {
+					max = height;
+					sMax = person;
+				}
+				++count;
+			}
+
+			avg = avg / count;
+
+			System.out.printf("%s: %f/%s (min) %f (avg) %f/%s (max) %d (count)\n", bsPais.getValue("pais"), min, sMin,
+					avg, max, sMax, count);
+		}
+	}
+}
